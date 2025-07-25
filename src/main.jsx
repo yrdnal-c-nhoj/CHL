d79fd3ecbacdece48e14dc000964b487ab538c4c
john-c-landry <johnclandry@yahoo.com>
2025-07-25 14:27:21 -0400

    x

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { Analytics } from "@vercel/analytics/next"
 
 
 createRoot(document.getElementById('root')).render(
