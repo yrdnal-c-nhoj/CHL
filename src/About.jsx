ed314f84ad6a1203d926e65ee3e5e13be8f85a41
john-c-landry <johnclandry@yahoo.com>
2025-07-26 16:23:30 -0400

    x

--- src/About.jsx
+++ src/About.jsx
@@ -12,8 +12,10 @@ function About() {
         <p>
         BorrowedTime is a new clock each day, using found images from the Internet. 
         </p><p>It is made with VSCode using React JavaScript, housed on Github and deployed on Vercel.</p>
-          <p>Previously this was an HTMl based site. Some of the clocks have not yet beem converted successfully to Java Script and can be found here on the old site.
-        We know that you depend on Cubist Heart Laboratories for your varied timekeeping needs. Whether you are launching spacecraft, splitting atoms, or timing delivery room contractions, we are here to help.
+          <p>Previously this was an HTMl based site. Some of the clocks have not yet beem converted successfully to Java Script and can be found <a href="https://github.com/the-clocks/Borrowed-Time">here</a> on the old site. 
+            </p>
+            <p>We are working hard to bring all your old favorites back while daily production continues.</p>
+         <p> We know that you depend on Cubist Heart Laboratories for your varied timekeeping needs. Whether you are launching spacecraft, splitting atoms, or timing delivery room contractions, we are your source for computationally intensive timekeeping devices.
         </p>
       </div>
  <Footer />
