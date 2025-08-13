039b56d95d84d03973910a4aadbffc908984a3f9
john-c-landry <johnclandry@yahoo.com>
2025-08-13 08:59:44 -0400

    x

--- src/pages/25-08-12/Clock.jsx
+++ src/pages/25-08-12/Clock.jsx
@@ -4,10 +4,10 @@ import customFontUrl from './cubic.ttf'; // local font file in same folder
 const faceColors = [
   'rgba(132, 87, 84, 0.75)',
   'rgba(113, 94, 107, 0.75)',
-  'rgba(139, 94, 60, 0.75)',
-  'rgba(107, 65, 47, 0.75)',
-  'rgba(113, 53, 5, 0.75)',
-  'rgba(148, 116, 77, 0.75)'
+  'rgba(89, 34, 30, 0.75)',
+  'rgba(137, 65, 47, 0.75)',
+  'rgba(73, 23, 25, 0.75)',
+  'rgba(148, 126, 77, 0.75)'
 ];
 
 const BiteviteHexahedron = () => {
@@ -36,21 +36,21 @@ const BiteviteHexahedron = () => {
     alignItems: 'center',
     justifyContent: 'center',
     overflow: 'hidden',
-    backgroundColor: '#4B3104FF'
+    backgroundColor: '#1F1504FF'
   };
 
   const perspectiveStyle = {
     position: 'relative',
     width: '25rem',
     height: '25rem',
-    perspective: '62.5rem'
+    perspective: '290rem'
   };
 
   const cubeStyle = {
     width: '100%',
     height: '100%',
     transformStyle: 'preserve-3d',
-    animation: 'biteviteRotate 20s infinite linear'
+    animation: 'biteviteRotate 120s infinite linear'
   };
 
   const baseFaceStyle = {
@@ -79,7 +79,7 @@ const BiteviteHexahedron = () => {
       0 -1px 2px rgba(255,255,255,0.05)
     `,
     filter: 'contrast(1.2) brightness(0.9)',
-    letterSpacing: '0.05em'
+    letterSpacing: '0.01em'
   };
 
   const faceTransforms = {
@@ -115,18 +115,57 @@ const BiteviteHexahedron = () => {
       <div style={containerStyle}>
         <div style={perspectiveStyle}>
           <div style={cubeStyle}>
-            {['front', 'back', 'right', 'left', 'top', 'bottom'].map((face, i) => (
-              <div
-                key={face}
-                style={{
-                  ...baseFaceStyle,
-                  transform: faceTransforms[face],
-                  backgroundColor: faceColors[i]
-                }}
-              >
-                <div style={timeDisplayStyle}>{timeString}</div>
-              </div>
-            ))}
+           {['front', 'back', 'right', 'left', 'top', 'bottom'].map((face, i) => (
+  <div
+    key={face}
+    style={{
+      ...baseFaceStyle,
+      transform: faceTransforms[face],
+      backgroundColor: faceColors[i]
+    }}
+  >
+    <div style={timeDisplayStyle}>{timeString}</div>
+
+    {/* 3D edges */}
+    {/* top edge */}
+    <div style={{
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '0.1rem',
+      backgroundColor: 'black'
+    }} />
+    {/* bottom edge */}
+    <div style={{
+      position: 'absolute',
+      bottom: 0,
+      left: 0,
+      width: '100%',
+      height: '0.1rem',
+      backgroundColor: 'black'
+    }} />
+    {/* left edge */}
+    <div style={{
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '0.1rem',
+      height: '100%',
+      backgroundColor: 'black'
+    }} />
+    {/* right edge */}
+    <div style={{
+      position: 'absolute',
+      top: 0,
+      right: 0,
+      width: '0.1rem',
+      height: '100%',
+      backgroundColor: 'black'
+    }} />
+  </div>
+))}
+
           </div>
         </div>
       </div>
