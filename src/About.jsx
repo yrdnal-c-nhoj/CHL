74157e3d55dbe291a423f60dc0423bef689f9289
john-c-landry <johnclandry@yahoo.com>
2025-08-20 19:37:02 -0400

    x

--- src/About.jsx
+++ src/About.jsx
@@ -23,25 +23,26 @@ function About() {
         <section className="manifestoSection">
           <p>
             <span className="hat">About Borrowed Time</span>
-            <p>&nbsp;</p> BorrowedTime is a new clock each day, using found images from the Internet.
+            <p>&nbsp;</p> BorrowedTime is a project by Cubist Heart Laboratories. It is new clock each day, using found images from the Internet.
           It is made with VSCode using React JavaScript, housed on Github and deployed on Vercel.
-          </p>
-
-          <p>
-          Previously this was an HTML-based website. You can see it<a href="https://the-clocks.github.io/Borrowed-Time/" target="_blank" rel="noopener noreferrer">here.</a>
           </p>
 
          
 
           <p>
            Whether you are launching spacecraft, splitting atoms, or timing delivery room contractions, we are your source for computationally intensive timekeeping devices.
+
+          <p>
+          Previously this was an HTML-based website. You can see it<a href="https://the-clocks.github.io/Borrowed-Time/" target="_blank" rel="noopener noreferrer">here.</a>
+          </p>
+
           </p>
 <br />
           <p>
             <span className="hat">About Cubist Heart Laboratories</span>
             <p>&nbsp;</p> 
-            A global collective of artists and scientists, Cubist Heart Laboratories is pushing the boundaries of computation and visual expression with genre-defying, boundary-breaking synergistically holistic projects involving emerging technologies.
-          </p>
+Cubist Heart Laboratories is a global collective of scientists, artists and shamans, found wherever electrons move.
+                 </p>
         </section>
       </div>
 
