ffe9361e2a4504c2174ae4a483a166c706595691
john-c-landry <johnclandry@yahoo.com>
2025-09-12 09:27:42 -0400

    x

--- src/pages/25-09-12/Clock.jsx
+++ src/pages/25-09-12/Clock.jsx
@@ -86,7 +86,7 @@ const Clock = () => {
   const secondRef = useRef(null);
 
   useEffect(() => {
-    const size = Math.min(window.innerWidth, window.innerHeight) * 0.9;
+    const size = Math.min(window.innerWidth, window.innerHeight) * 1.0;
     const dpr = window.devicePixelRatio || 1;
 
     const createCanvasContext = (ref) => {
