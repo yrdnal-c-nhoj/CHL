0f3aa949daf3860f66103c94572fe52edc5097b7
john-c-landry <johnclandry@yahoo.com>
2025-10-10 13:16:34 -0400

    x

--- scripts/screenshot.mjs
+++ scripts/screenshot.mjs
@@ -7,7 +7,7 @@ const screenshotsDir = '/Users/john/Desktop/CHL/screenshots';
 if (!fs.existsSync(screenshotsDir)) fs.mkdirSync(screenshotsDir, { recursive: true });
 
 // Paths you want to capture
-const paths = ['today'];
+const paths = ['25-10-09'];
 const baseURL = 'https://www.cubistheart.com/';
 
 // Delay in milliseconds before screenshot (e.g., 3000 = 3s)
