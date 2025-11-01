dcb374ae1f1df9a53c3472a45e89cf9cc29ade0e
john-c-landry <johnclandry@yahoo.com>
2025-11-01 15:50:05 -0400

    z

--- src/pages/25-04-05/Clock.jsx
+++ src/pages/25-04-05/Clock.jsx
@@ -27,13 +27,12 @@ const TallClock = () => {
 
     updateClock();
     const interval = setInterval(updateClock, 1000);
-
     return () => clearInterval(interval);
   }, []);
 
   return (
     <div style={styles.container}>
-      <div style={styles.clock} className="clock" aria-label="Analog clock">
+      <div style={styles.clock} className="clock">
         <div className="hand hour-hand" style={styles.hand}>
           <img src={hourHand} alt="Hour Hand" style={styles.handImg} />
         </div>
@@ -44,6 +43,7 @@ const TallClock = () => {
           <img src={secondHand} alt="Second Hand" style={styles.handImg} />
         </div>
       </div>
+
       <div style={{ ...styles.overlay, ...styles.overlay1 }} />
       <div style={{ ...styles.overlay, ...styles.overlay2 }} />
       <div style={{ ...styles.overlay, ...styles.overlay3 }} />
@@ -56,28 +56,32 @@ const styles = {
     backgroundColor: "#805c0d",
     margin: 0,
     padding: 0,
-    height: "100dvh",
+    height: "100dvh", // Chrome-safe dynamic viewport
     width: "100vw",
     overflow: "hidden",
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
     position: "relative",
+    touchAction: "none",
   },
   clock: {
     position: "relative",
-    width: "22vw",
-    height: "22vh",
+    width: "min(60vw, 40vh)", // keep it responsive
+    height: "min(60vw, 40vh)",
+    maxWidth: "90vmin",
+    maxHeight: "90vmin",
   },
   hand: {
     position: "absolute",
-    top: 0,
-    left: 0,
-    width: "100%",
-    height: "88%",
+    top: "5%",
+    left: "50%",
     transformOrigin: "50% 90%",
-    pointerEvents: "none",
+    width: "100%",
+    height: "100%",
+    translate: "-50% 0",
     zIndex: 9,
+    pointerEvents: "none",
   },
   handImg: {
     width: "100%",
@@ -94,17 +98,21 @@ const styles = {
     backgroundRepeat: "repeat",
     zIndex: 1,
     pointerEvents: "none",
+    backgroundAttachment: "fixed", // prevents weird parallax on scroll
   },
   overlay1: {
-    backgroundSize: "15vh 15vw",
+    backgroundSize: "12vmin 12vmin",
+    opacity: 0.8,
     zIndex: 3,
   },
   overlay2: {
-    backgroundSize: "10vh 10vw",
+    backgroundSize: "20vmin 20vmin",
+    opacity: 0.5,
     zIndex: 2,
   },
   overlay3: {
     backgroundSize: "auto",
+    opacity: 0.3,
     zIndex: 1,
   },
 };
