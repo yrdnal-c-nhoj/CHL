d0cecc08803b28531901914f9bd79ad012fd5f40
john-c-landry <johnclandry@yahoo.com>
2025-10-31 21:45:16 -0400

    x

--- src/pages/25-05-11/Clock.jsx
+++ src/pages/25-05-11/Clock.jsx
@@ -45,106 +45,148 @@ const AquariumClock = () => {
     position: 'fixed',
     top: 0,
     left: 0,
-    margin: 0,
-    padding: 0,
     width: '100vw',
     height: '100dvh',
   };
 
   return (
     <div>
-      <img src={spin} style={{
-        ...sharedImageStyle,
-        opacity: 0.6,
-        height: '80vh',
-        zIndex: 3,
-        filter: 'sepia(100%) hue-rotate(-30deg) brightness(120%) contrast(110%) saturate(400%)'
-      }} alt="spin" />
-      <img src={spin} style={{
-        ...sharedImageStyle,
-        transform: 'scaleX(-1)',
-        opacity: 0.6,
-        height: '80vh',
-        zIndex: 4,
-        filter: 'sepia(100%) hue-rotate(-30deg) brightness(120%) contrast(110%) saturate(400%)'
-      }} alt="spin mirrored" />
-      <img src={bubl} style={{
-        position: 'fixed',
-        top: 0,
-        left: '-22vw',
-        width: '100%',
-        height: '110%',
-        zIndex: 9,
-        pointerEvents: 'none'
-      }} alt="bubbles" />
-      <img src={bubl} style={{
-        position: 'absolute',
-        bottom: 0
-      }} alt="bubbles 2" />
+      {/* Background Aquarium */}
+      <img
+        src={aquarium}
+        style={{ ...sharedImageStyle, opacity: 0.5, zIndex: 0 }}
+        alt="aquarium forward"
+      />
+      <img
+        src={aquarium}
+        style={{ ...sharedImageStyle, opacity: 0.9, transform: 'scaleX(-1)', zIndex: 1 }}
+        alt="aquarium backward"
+      />
 
-      <div style={{
-        width: '80vw',
-        height: '100vh',
-        position: 'absolute',
-        top: '50%',
-        left: '50%',
-        transform: 'translate(-50%, -50%)',
-        zIndex: 8
-      }}>
-        <img src={fish} className="hand hour-hand" style={{
-          position: 'absolute',
-          width: '20%',
-          height: '30%',
-          right: '50%',
-          top: '40%',
-          transformOrigin: '100%',
-          rotate: '30deg',
-          filter: 'drop-shadow(2px 1px 3px rgb(0, 3, 2)) drop-shadow(-1px 1px 1px rgb(6, 85, 31)) drop-shadow(1px -1px 1px rgb(10, 154, 109)) drop-shadow(-1px -1px 1px rgb(214, 227, 216))',
-        }} alt="hour hand" />
+      {/* Spin effects */}
+      <img
+        src={spin}
+        style={{
+          ...sharedImageStyle,
+          opacity: 0.6,
+          height: '80vh',
+          zIndex: 2,
+          filter: 'sepia(100%) hue-rotate(-30deg) brightness(120%) contrast(110%) saturate(400%)',
+        }}
+        alt="spin"
+      />
+      <img
+        src={spin}
+        style={{
+          ...sharedImageStyle,
+          transform: 'scaleX(-1)',
+          opacity: 0.6,
+          height: '80vh',
+          zIndex: 3,
+          filter: 'sepia(100%) hue-rotate(-30deg) brightness(120%) contrast(110%) saturate(400%)',
+        }}
+        alt="spin mirrored"
+      />
 
-        <img src={fish} className="hand min-hand" style={{
+      {/* Floating bubbles */}
+      <img
+        src={bubl}
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: '-22vw',
+          width: '100%',
+          height: '110%',
+          zIndex: 4,
+          pointerEvents: 'none',
+        }}
+        alt="bubbles"
+      />
+      <img
+        src={bubl}
+        style={{
           position: 'absolute',
-          width: '40%',
-          height: '30%',
-          right: '50%',
-          top: '40%',
-          transformOrigin: '100%',
-          filter: 'drop-shadow(2px 1px 3px rgb(0, 3, 2)) drop-shadow(-1px 1px 1px rgb(6, 85, 31)) drop-shadow(1px -1px 1px rgb(10, 154, 109)) drop-shadow(-1px -1px 1px rgb(214, 227, 216))',
-        }} alt="minute hand" />
+          bottom: 0,
+          zIndex: 5,
+        }}
+        alt="bubbles 2"
+      />
 
-        <img src={fish} className="hand second-hand" style={{
+      {/* Clock hands */}
+      <div
+        style={{
+          width: '80vw',
+          height: '100vh',
           position: 'absolute',
-          width: '65%',
-          height: '20%',
-          right: '50%',
-          top: '40%',
-          transformOrigin: '100%',
-          filter: 'drop-shadow(2px 1px 3px rgb(0, 3, 2)) drop-shadow(-1px 1px 1px rgb(6, 85, 31)) drop-shadow(1px -1px 1px rgb(10, 154, 109)) drop-shadow(-1px -1px 1px rgb(214, 227, 216))',
-        }} alt="second hand" />
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          zIndex: 6,
+        }}
+      >
+        <img
+          src={fish}
+          className="hand hour-hand"
+          style={{
+            position: 'absolute',
+            width: '20%',
+            height: '30%',
+            right: '50%',
+            top: '40%',
+            transformOrigin: '100%',
+            rotate: '30deg',
+            filter:
+              'drop-shadow(2px 1px 3px rgb(0, 3, 2)) drop-shadow(-1px 1px 1px rgb(6, 85, 31)) drop-shadow(1px -1px 1px rgb(10, 154, 109)) drop-shadow(-1px -1px 1px rgb(214, 227, 216))',
+          }}
+          alt="hour hand"
+        />
+        <img
+          src={fish}
+          className="hand min-hand"
+          style={{
+            position: 'absolute',
+            width: '40%',
+            height: '30%',
+            right: '50%',
+            top: '40%',
+            transformOrigin: '100%',
+            filter:
+              'drop-shadow(2px 1px 3px rgb(0, 3, 2)) drop-shadow(-1px 1px 1px rgb(6, 85, 31)) drop-shadow(1px -1px 1px rgb(10, 154, 109)) drop-shadow(-1px -1px 1px rgb(214, 227, 216))',
+          }}
+          alt="minute hand"
+        />
+        <img
+          src={fish}
+          className="hand second-hand"
+          style={{
+            position: 'absolute',
+            width: '65%',
+            height: '20%',
+            right: '50%',
+            top: '40%',
+            transformOrigin: '100%',
+            filter:
+              'drop-shadow(2px 1px 3px rgb(0, 3, 2)) drop-shadow(-1px 1px 1px rgb(6, 85, 31)) drop-shadow(1px -1px 1px rgb(10, 154, 109)) drop-shadow(-1px -1px 1px rgb(214, 227, 216))',
+          }}
+          alt="second hand"
+        />
       </div>
 
-      <img src={gfish} style={{
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        width: '180vw',
-        height: '100vh',
-        opacity: 0.8,
-        transform: 'scaleX(-1)',
-        zIndex: 4
-      }} alt="gfish" />
-
-      <img src={aquarium} style={{
-        ...sharedImageStyle,
-        opacity: 0.5,
-        zIndex: -4
-      }} alt="aquarium forward" />
-      <img src={aquarium} style={{
-        ...sharedImageStyle,
-        opacity: 0.9,
-        transform: 'scaleX(-1)',
-        zIndex: -5
-      }} alt="aquarium backward" />
+      {/* Foreground fish */}
+      <img
+        src={gfish}
+        style={{
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '180vw',
+          height: '100vh',
+          opacity: 0.8,
+          transform: 'scaleX(-1)',
+          zIndex: 7,
+        }}
+        alt="gfish"
+      />
     </div>
   );
 };
