945b6842694df4cb376219ab92af8b235751bc13
john-c-landry <johnclandry@yahoo.com>
2025-11-03 10:18:12 -0500

    x

--- src/pages/25-05-19/Clock.jsx
+++ src/pages/25-05-19/Clock.jsx
@@ -1,8 +1,7 @@
-// Clock.jsx
 import { useEffect, useState } from 'react';
 import antFontUrl from './Ant.ttf';
-import bg1 from "./ants.gif"; // tiling
-import bg2 from "./ants1.gif"; // cover
+import bg1 from "./ants.gif";
+import bg2 from "./ants1.gif";
 
 const Clock = () => {
   const [time, setTime] = useState(new Date());
@@ -20,112 +19,32 @@ const Clock = () => {
   const minuteDeg = (minutes + seconds / 60) * 6;
   const secondDeg = seconds * 6;
 
+  const radius = 45; // percent from center for numbers
+
   return (
     <>
-      <style>
-        {`
+      <style>{`
         @font-face {
           font-family: 'Ant';
           src: url(${antFontUrl}) format('truetype');
         }
-
         body, #root {
           margin: 0;
           padding: 0;
           height: 100%;
         }
+      `}</style>
 
-        .container {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          height: 100dvh;
-          width: 100vw;
-          background-color: rgb(255, 254, 254);
-          position: relative;
-          overflow: hidden;
-        }
-
-        .clock {
-          width: 60vh;
-          height: 60vh;
-          max-width: 400px;
-          max-height: 400px;
-          min-width: 200px;
-          min-height: 200px;
-          border-radius: 50%;
-          position: relative;
-          z-index: 4;
-        }
-
-        .number {
-          font-family: 'Ant', sans-serif;
-          position: absolute;
-          width: 100%;
-          height: 100%;
-          text-align: center;
-          font-size: 22vh;
-        }
-
-        .number span {
-          display: inline-block;
-          transform: translateY(-17.5vw);
-        }
-
-        .hand {
-          position: absolute;
-          bottom: 50%;
-          left: 50%;
-          transform-origin: bottom;
-          z-index: 10;
-          transform: translateX(-50%);
-        }
-
-        .hour-hand {
-          width: 0.5vw;
-          height: 29vw;
-          background-color: black;
-          max-width: 8px;
-          max-height: 80px;
-          min-width: 4px;
-          min-height: 40px;
-        }
-
-        .minute-hand {
-          width: 0.4vw;
-          height: 25vw;
-          background-color: black;
-          max-width: 6px;
-          max-height: 100px;
-          min-width: 3px;
-          min-height: 50px;
-        }
-
-        .second-hand {
-          width: 0.3vw;
-          height: 50vw;
-          background-color: rgb(9, 9, 9);
-          max-width: 4px;
-          max-height: 120px;
-          min-width: 2px;
-          min-height: 60px;
-        }
-
-        .center {
-          width: 10px;
-          height: 10px;
-          background-color: black;
-          border-radius: 50%;
-          position: absolute;
-          left: 50%;
-          top: 50%;
-          transform: translate(-50%, -50%);
-          z-index: 11;
-        }
-      `}
-      </style>
-
-      <div className="container">
+      <div style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100vw',
+        height: '100dvh',
+        position: 'relative',
+        overflow: 'hidden',
+        backgroundColor: 'rgb(255,254,254)'
+      }}>
         {/* Backgrounds */}
         <div style={{
           backgroundImage: `url(${bg2})`,
@@ -146,23 +65,88 @@ const Clock = () => {
         }} />
 
         {/* Clock */}
-        <div className="clock">
+        <div style={{
+          position: 'relative',
+          width: '60vh',
+          height: '60vh',
+          minWidth: '200px',
+          minHeight: '200px',
+          maxWidth: '400px',
+          maxHeight: '400px',
+          borderRadius: '50%',
+          zIndex: 4,
+        }}>
+          {/* Numbers */}
           {[...Array(12)].map((_, i) => {
             const angle = (i + 1) * 30;
+            const rad = angle * Math.PI / 180;
             return (
-              <div className="number" key={i} style={{ transform: `rotate(${angle}deg)` }}>
-                <span style={{ color: 'black' }}>{i + 1}</span>
+              <div key={i} style={{
+                position: 'absolute',
+                left: `${50 + radius * Math.sin(rad)}%`,
+                top: `${50 - radius * Math.cos(rad)}%`,
+                transform: 'translate(-50%, -50%)',
+                fontFamily: 'Ant, sans-serif',
+                fontSize: '18vh',
+                color: 'black'
+              }}>
+                {i + 1}
               </div>
             );
           })}
-          <div className="hand hour-hand" style={{ transform: `translateX(-50%) rotate(${hourDeg}deg)` }} />
-          <div className="hand minute-hand" style={{ transform: `translateX(-50%) rotate(${minuteDeg}deg)` }} />
-          <div className="hand second-hand" style={{ transform: `translateX(-50%) rotate(${secondDeg}deg)` }} />
-          <div className="center" />
-        </div>
 
-        {/* Content */}
-      
+          {/* Hour Hand */}
+          <div style={{
+            position: 'absolute',
+            width: '1vh',
+            height: '20vh',
+            backgroundColor: 'black',
+            top: '50%',
+            left: '50%',
+            transform: `translate(-50%, -50%) rotate(${hourDeg}deg)`,
+            transformOrigin: '50% 50%',
+            borderRadius: '1vh'
+          }} />
+
+          {/* Minute Hand */}
+          <div style={{
+            position: 'absolute',
+            width: '0.8vh',
+            height: '28vh',
+            backgroundColor: 'black',
+            top: '50%',
+            left: '50%',
+            transform: `translate(-50%, -50%) rotate(${minuteDeg}deg)`,
+            transformOrigin: '50% 50%',
+            borderRadius: '1vh'
+          }} />
+
+          {/* Second Hand */}
+          <div style={{
+            position: 'absolute',
+            width: '0.5vh',
+            height: '30vh',
+            backgroundColor: 'red',
+            top: '50%',
+            left: '50%',
+            transform: `translate(-50%, -50%) rotate(${secondDeg}deg)`,
+            transformOrigin: '50% 50%',
+            borderRadius: '1vh'
+          }} />
+
+          {/* Center Dot */}
+          <div style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            width: '2vh',
+            height: '2vh',
+            backgroundColor: 'black',
+            borderRadius: '50%',
+            transform: 'translate(-50%, -50%)',
+            zIndex: 10
+          }} />
+        </div>
       </div>
     </>
   );
