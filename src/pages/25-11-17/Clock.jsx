2d8fc5d16fc58f6ef75c7a719a307191d908e1e6
john-c-landry <johnclandry@yahoo.com>
2025-11-18 20:53:26 -0500

    x

--- src/pages/25-11-17/Clock.jsx
+++ src/pages/25-11-17/Clock.jsx
@@ -60,7 +60,7 @@ export default function MarsDigitalClock() {
       left: 0,
       width: "100%",
       height: "100%",
-      background: "linear-gradient(185deg, #ff0000, #CE3A0DFF)",
+      background: "linear-gradient(185deg, #F80606FF, #CE3A0DFF)",
       zIndex: 0,
     },
     background1: {
