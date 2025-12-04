6c1e8ab644c3c7cb074874749d9a9b519ebf1ff5
john-c-landry <johnclandry@yahoo.com>
2025-12-04 09:23:17 -0500

    x

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
-
-export default {
-  plugins: [tailwindcss(), autoprefixer()],
-};
+module.exports = {
+  plugins: [
+    require('@tailwindcss/postcss'),
+    require('autoprefixer')
+  ]
+}
