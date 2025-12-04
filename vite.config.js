bef93dfd169029347028e0b59f06816558c10ca4
john-c-landry <johnclandry@yahoo.com>
2025-12-04 09:16:25 -0500

    x

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import mdx from '@mdx-js/rollup';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    mdx(),
-  ],
+  plugins: [react()],
   css: {
-    postcss: {
-      plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-      ],
-    },
+    postcss: './postcss.config.js', // use separate PostCSS config
   },
 });
