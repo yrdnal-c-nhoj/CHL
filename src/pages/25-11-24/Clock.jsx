b9ff64b67cc2224408309dd0f604ee8e2861e923
john-c-landry <johnclandry@yahoo.com>
2025-12-13 04:48:11 -0500

    h

--- src/pages/25-11-24/Clock.jsx
+++ src/pages/25-11-24/Clock.jsx
@@ -1,39 +1,39 @@
-import React, { useEffect, useState, useRef } from "react";
-import pageBgImgBase from "./skin.jpg";
-import pageBgImg from "./sss.webp";
-import clockFaceImg from "./sn.gif";
-import hourHandImg from "./sn5.webp";
-import minuteHandImg from "./sfsd.webp";
-import secondHandImg from "./sn1.webp";
-import clockCenterImg from "./center.webp"; // <-- your new center image
-import fontFile from "./snake.ttf";
+import React, { useEffect, useState, useRef } from 'react'
+import pageBgImgBase from './skin.jpg'
+import pageBgImg from './sss.webp'
+import clockFaceImg from './sn.gif'
+import hourHandImg from './sn5.webp'
+import minuteHandImg from './sfsd.webp'
+import secondHandImg from './sn1.webp'
+import clockCenterImg from './center.webp' // <-- your new center image
+import fon251124 from './snake.ttf'
 
-export default function AnalogClock() {
-  const [time, setTime] = useState(new Date());
-  const requestRef = useRef();
+export default function AnalogClock () {
+  const [time, setTime] = useState(new Date())
+  const requestRef = useRef()
 
   // Smooth update with requestAnimationFrame
   const updateTime = () => {
-    setTime(new Date());
-    requestRef.current = requestAnimationFrame(updateTime);
-  };
+    setTime(new Date())
+    requestRef.current = requestAnimationFrame(updateTime)
+  }
 
   useEffect(() => {
-    requestRef.current = requestAnimationFrame(updateTime);
-    return () => cancelAnimationFrame(requestRef.current);
-  }, []);
+    requestRef.current = requestAnimationFrame(updateTime)
+    return () => cancelAnimationFrame(requestRef.current)
+  }, [])
 
-  const hours = time.getHours() % 12;
-  const minutes = time.getMinutes();
-  const seconds = time.getSeconds() + time.getMilliseconds() / 1000;
+  const hours = time.getHours() % 12
+  const minutes = time.getMinutes()
+  const seconds = time.getSeconds() + time.getMilliseconds() / 1000
 
-  const hourAngle = (hours + minutes / 60) * 30;
-  const minuteAngle = (minutes + seconds / 60) * 6;
-  const secondAngle = seconds * 6;
+  const hourAngle = (hours + minutes / 60) * 30
+  const minuteAngle = (minutes + seconds / 60) * 6
+  const secondAngle = seconds * 6
 
-  const unit = "vmin"; 
-  const clockSize = 100; // smaller size now
-  const tileSize = 25;
+  const unit = 'vmin'
+  const clockSize = 100 // smaller size now
+  const tileSize = 25
 
   const pageWrapperStyle = {
     position: 'fixed',
@@ -44,8 +44,8 @@ export default function AnalogClock() {
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    overflow: 'hidden',
-  };
+    overflow: 'hidden'
+  }
 
   const pageBackgroundBase = {
     position: 'absolute',
@@ -57,9 +57,9 @@ export default function AnalogClock() {
     backgroundSize: 'cover',
     backgroundPosition: 'center',
     backgroundRepeat: 'no-repeat',
-    filter: "saturate(2.7) contrast(0.4) brightness(0.4)",
-    zIndex: -3,
-  };
+    filter: 'saturate(2.7) contrast(0.4) brightness(0.4)',
+    zIndex: -3
+  }
 
   const pageBackgroundLayer1 = {
     position: 'absolute',
@@ -69,79 +69,79 @@ export default function AnalogClock() {
     height: '100%',
     backgroundImage: `url(${pageBgImg})`,
     backgroundRepeat: 'repeat',
-    filter: "saturate(2.7)",
+    filter: 'saturate(2.7)',
     opacity: 0.6,
     backgroundSize: `${tileSize}${unit} ${tileSize}${unit}`,
     backgroundPosition: 'center',
-    zIndex: -2,
-  };
+    zIndex: -2
+  }
 
   const containerStyle = {
     position: 'relative',
     width: `${clockSize}${unit}`,
     height: `${clockSize}${unit}`,
-    borderRadius: "50%",
-    overflow: "visible",
-    textAlign: "center",
-  };
+    borderRadius: '50%',
+    overflow: 'visible',
+    textAlign: 'center'
+  }
 
   const backgroundStyle = {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
     left: 0,
-    width: "100%",
-    height: "100%",
-    borderRadius: "50%",
+    width: '100%',
+    height: '100%',
+    borderRadius: '50%',
     backgroundImage: `url(${clockFaceImg})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    filter: "saturate(0.7) contrast(1.4) brightness(0.7)",
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    filter: 'saturate(0.7) contrast(1.4) brightness(0.7)',
     opacity: 0.6,
-    zIndex: 1,
-  };
+    zIndex: 1
+  }
 
   const handStyle = (angle, length) => ({
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    width: "auto",
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    width: 'auto',
     height: `${length}${unit}`,
     transform: `translate(-50%, -100%) rotate(${angle}deg)`,
-    transformOrigin: "50% 100%",
-  });
+    transformOrigin: '50% 100%'
+  })
 
   const fontBlob = `@font-face {
     font-family: 'customFont';
-    src: url(${fontFile}) format('truetype');
+    src: url(${fon251124}) format('truetype');
     font-weight: normal;
     font-style: normal;
-  }`;
+  }`
 
-  const digits = Array.from({ length: 12 }, (_, i) => i + 1);
+  const digits = Array.from({ length: 12 }, (_, i) => i + 1)
 
-  const digitStyle = (num) => {
-    const angle = (num * 30 - 90) * (Math.PI / 180);
-    const radius = clockSize / 2 - 7;
-    const x = radius * Math.cos(angle);
-    const y = radius * Math.sin(angle);
-    
+  const digitStyle = num => {
+    const angle = (num * 30 - 90) * (Math.PI / 180)
+    const radius = clockSize / 2 - 7
+    const x = radius * Math.cos(angle)
+    const y = radius * Math.sin(angle)
+
     return {
-      position: "absolute",
-      top: "50%",
-      left: "50%",
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
       transform: `translate(${x}${unit}, ${y}${unit}) translate(-50%, -50%)`,
-      fontFamily: "customFont",
+      fontFamily: 'customFont',
       fontSize: `10${unit}`,
-      color: "#6C5D35D0",
-      textShadow: "1px 1px rgba(220,192,192,0.9), -1px -1px rgba(0,0,0,0.9)",
-      userSelect: "none",
-      zIndex: 2,
-    };
-  };
+      color: '#6C5D35D0',
+      textShadow: '1px 1px rgba(220,192,192,0.9), -1px -1px rgba(0,0,0,0.9)',
+      userSelect: 'none',
+      zIndex: 2
+    }
+  }
 
-  const hourHandLen = 42;
-  const minuteHandLen = 50;
-  const secondHandLen = 58;
+  const hourHandLen = 42
+  const minuteHandLen = 50
+  const secondHandLen = 58
 
   return (
     <div style={pageWrapperStyle}>
@@ -152,7 +152,7 @@ export default function AnalogClock() {
         <style>{fontBlob}</style>
         <div style={backgroundStyle}></div>
 
-        {digits.map((d) => (
+        {digits.map(d => (
           <div key={d} style={digitStyle(d)}>
             {d}
           </div>
@@ -161,56 +161,55 @@ export default function AnalogClock() {
         {/* Center image */}
         <img
           src={clockCenterImg}
-          alt="center"
+          alt='center'
           style={{
-            position: "absolute",
-            top: "52%",
-            left: "50%",
-            width: "22vmin",
-            height: "16vmin",
-            transform: "translate(-50%, -50%)",
+            position: 'absolute',
+            top: '52%',
+            left: '50%',
+            width: '22vmin',
+            height: '16vmin',
+            transform: 'translate(-50%, -50%)',
             zIndex: 9,
             filter:
-              "saturate(1.2) contrast(1.2) hue-rotate(44deg)  brightness(0.8) drop-shadow(0 1px 0.5vmin rgba(222,211,210))",
+              'saturate(1.2) contrast(1.2) hue-rotate(44deg)  brightness(0.8) drop-shadow(0 1px 0.5vmin rgba(222,211,210))',
 
-            pointerEvents: "none"
+            pointerEvents: 'none'
           }}
         />
 
-        <img 
-          src={hourHandImg} 
-          alt="hour" 
-          style={{ 
+        <img
+          src={hourHandImg}
+          alt='hour'
+          style={{
             ...handStyle(hourAngle, hourHandLen),
             zIndex: 8,
             filter:
-              "saturate(1.2) contrast(1.9) brightness(0.8) drop-shadow(0 1px 0.5vmin rgba(222,211,210))"
-          }} 
+              'saturate(1.2) contrast(1.9) brightness(0.8) drop-shadow(0 1px 0.5vmin rgba(222,211,210))'
+          }}
         />
 
-        <img 
-          src={minuteHandImg} 
-          alt="minute" 
-          style={{ 
+        <img
+          src={minuteHandImg}
+          alt='minute'
+          style={{
             ...handStyle(minuteAngle, minuteHandLen),
             zIndex: 6,
             filter:
-              "saturate(1.1) contrast(1.2) brightness(0.9) drop-shadow(0 1px 0.5vmin rgba(222,211,210))"
-          }} 
+              'saturate(1.1) contrast(1.2) brightness(0.9) drop-shadow(0 1px 0.5vmin rgba(222,211,210))'
+          }}
         />
 
-        <img 
-          src={secondHandImg} 
-          alt="second" 
-          style={{ 
+        <img
+          src={secondHandImg}
+          alt='second'
+          style={{
             ...handStyle(secondAngle, secondHandLen),
             zIndex: 3,
             filter:
-              "grayscale(100%) sepia(100%) hue-rotate(44deg) saturate(50%) contrast(1.7) brightness(0.9) drop-shadow(0 1px 0.5vmin rgba(222,211,210))"
-          }} 
+              'grayscale(100%) sepia(100%) hue-rotate(44deg) saturate(50%) contrast(1.7) brightness(0.9) drop-shadow(0 1px 0.5vmin rgba(222,211,210))'
+          }}
         />
-
       </div>
     </div>
-  );
+  )
 }
