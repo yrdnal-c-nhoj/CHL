b687602fd666e31be8db2b27f490d6d7a780d8c0
john-c-landry <johnclandry@yahoo.com>
2025-12-16 13:00:46 -0500

    x

--- src/pages/25-12-16/Clock.jsx
+++ src/pages/25-12-16/Clock.jsx
@@ -35,9 +35,11 @@ const QuadClock = () => {
     alignItems: 'center',
     height: '100vh',
     width: '100vw',
-    backgroundColor: '#F1E870FF',
+    backgroundColor: '#754C09FF',
     backgroundImage:
       "url(\"data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M15 0C6.716 0 0 6.716 0 15c8.284 0 15-6.716 15-15zM0 15c0 8.284 6.716 15 15 15 0-8.284-6.716-15-15-15zm30 0c0-8.284-6.716-15-15-15 0 8.284 6.716 15 15 15zm0 0c0 8.284-6.716 15-15 15 0-8.284 6.716-15 15-15z' fill='%239C92AC' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E\")",
+    backgroundPosition: 'center',
+    backgroundRepeat: 'repeat',
     margin: 0,
     overflow: 'hidden',
     fontFamily: 'font_2025_12_16, system-ui, sans-serif'
@@ -147,7 +149,7 @@ const QuadClock = () => {
             top: '0',
             bottom: '0',
             width: '1px',
-            backgroundColor: 'rgba(25,255,255,0.6)'
+            backgroundColor: '#BEBCBCFF'
             // zIndex: 1
           }}
         />
@@ -158,7 +160,7 @@ const QuadClock = () => {
             left: '0',
             right: '0',
             height: '1px',
-            backgroundColor: 'rgba(25,255,255,0.6)'
+            backgroundColor: '#B0ADADFF'
             // zIndex: 1
           }}
         />
