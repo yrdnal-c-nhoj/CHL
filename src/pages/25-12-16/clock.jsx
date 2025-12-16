44150bf2705163746ed3e878b6b593f521993430
john-c-landry <johnclandry@yahoo.com>
2025-12-16 10:49:45 -0500

    x

--- src/pages/25-12-16/clock.jsx
+++ src/pages/25-12-16/clock.jsx
@@ -36,8 +36,8 @@ const QuadClock = () => {
     height: '100vh',
     width: '100vw',
     backgroundColor: '#754C09FF',
-    backgroundImage:
-      "url(\"data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M15 0C6.716 0 0 6.716 0 15c8.284 0 15-6.716 15-15zM0 15c0 8.284 6.716 15 15 15 0-8.284-6.716-15-15-15zm30 0c0-8.284-6.716-15-15-15 0 8.284 6.716 15 15 15zm0 0c0 8.284-6.716 15-15 15 0-8.284 6.716-15 15-15z' fill='%239C92AC' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E\")",
+    // backgroundImage:
+    //   "url(\"data:image/svg+xml,%3Csvg width='30' height='30' viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M15 0C6.716 0 0 6.716 0 15c8.284 0 15-6.716 15-15zM0 15c0 8.284 6.716 15 15 15 0-8.284-6.716-15-15-15zm30 0c0-8.284-6.716-15-15-15 0 8.284 6.716 15 15 15zm0 0c0 8.284-6.716 15-15 15 0-8.284 6.716-15 15-15z' fill='%239C92AC' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E\")",
     margin: 0,
     overflow: 'hidden',
     fontFamily: 'font_2025_12_16, system-ui, sans-serif'
