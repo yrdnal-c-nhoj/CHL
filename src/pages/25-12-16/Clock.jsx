197e1880c51f99bc9a7812cad74395d24f56d595
john-c-landry <johnclandry@yahoo.com>
2025-12-16 17:22:35 -0500

    z

--- src/pages/25-12-16/Clock.jsx
+++ src/pages/25-12-16/Clock.jsx
@@ -39,8 +39,7 @@ const QuadClock = () => {
     right: 0,
     bottom: 0,
     backgroundColor: '#475502FF',
-    backgroundImage:
-      "url(\"data:image/svg+xml,%3Csvg width='13' height='13' viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M15 0C6.716 0 0 6.716 0 15c8.284 0 15-6.716 15-15zM0 15c0 8.284 6.716 15 15 15 0-8.284-6.716-15-15-15zm30 0c0-8.284-6.716-15-15-15 0 8.284 6.716 15 15 15zm0 0c0 8.284-6.716 15-15 15 0-8.284 6.716-15 15-15z' fill='%239C92AC' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E\")",
+    backgroundImage: `radial-gradient(circle, #D0CC71CA, transparent), url("data:image/svg+xml,%3Csvg width='13' height='13' viewBox='0 0 30 30' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M15 0C6.716 0 0 6.716 0 15c8.284 0 15-6.716 15-15zM0 15c0 8.284 6.716 15 15 15 0-8.284-6.716-15-15-15zm30 0c0-8.284-6.716-15-15-15 0 8.284 6.716 15 15 15zm0 0c0 8.284-6.716 15-15 15 0-8.284 6.716-15 15-15z' fill='%239C92AC' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E")`,
     backgroundPosition: 'center',
     backgroundRepeat: 'repeat',
     margin: 0,
@@ -126,7 +125,7 @@ const QuadClock = () => {
             sDeg,
             `${CLOCK_SIZE * 0.45}vmin`,
             `${CLOCK_SIZE * 0.015}vmin`,
-            '#F60404FF'
+            '#FB6712FF'
           )}
         />
       </div>
