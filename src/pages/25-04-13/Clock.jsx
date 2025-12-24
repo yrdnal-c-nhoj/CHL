f62c40eff7eaa7b5df183bdc32d954f25afe2e11
john-c-landry <johnclandry@yahoo.com>
2025-12-24 09:55:38 -0500

    x

--- src/pages/25-04-13/Clock.jsx
+++ src/pages/25-04-13/Clock.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import backgroundImage from './images/pattern.jpeg';
-import pizzaFace from './images/pie.png';
+import pizzaFace from './images/pie.webp';
 import hourSlice from './images/slice3.png';
 import minuteSlice from './images/slice2.png';
 import secondSlice from './images/slice1.png';
