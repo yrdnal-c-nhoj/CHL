3ef71e4439ecd0c439a5304fa4b3dc109ecc2146
john-c-landry <johnclandry@yahoo.com>
2025-12-27 12:31:52 -0500

    s

--- src/pages/25-12-24/Clock.jsx
+++ src/pages/25-12-24/Clock.jsx
@@ -51,21 +51,29 @@ const DigitalClock = () => {
     fontFamily: `"${FONT_FAMILY}", monospace`,
   };
 
-  const Layer = ({ value, size, zIndex, opacity }) => (
-    <div style={{
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-40%, -50%)',
-      fontSize: size,
-      zIndex: zIndex,
-      opacity: opacity,
-      pointerEvents: 'none',
-      whiteSpace: 'nowrap'
-    }}>
-      {value}
-    </div>
-  );
+  const Layer = ({ value, size, zIndex, opacity }) => {
+    const leftPart = value.substring(0, 1);
+    const rightPart = value.substring(1);
+    
+    return (
+      <div style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        fontSize: size,
+        zIndex: zIndex,
+        opacity: opacity,
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        gap: '0.02em'
+      }}>
+        <div style={{ textAlign: 'right', width: '40%' }}>{leftPart}</div>
+        <div style={{ textAlign: 'left', width: '40%' }}>{rightPart}</div>
+      </div>
+    );
+  };
 
 
   return (
@@ -75,13 +83,13 @@ const DigitalClock = () => {
       */}
       
       {/* Seconds: 80% of screen height */}
-      <Layer value={seconds} size="80vh" zIndex={1} opacity={0.8} />
+      <Layer value={seconds} size="70vh" zIndex={1} opacity={0.7} />
       
       {/* Minutes: Half of seconds (40% of screen height) */}
-      <Layer value={minutes} size="60vh" zIndex={2} opacity={0.9} />
+      <Layer value={minutes} size="60vh" zIndex={2} opacity={0.8} />
       
       {/* Hours: Half of minutes (20% of screen height) */}
-      <Layer value={hours} size="40vh" zIndex={3} opacity={1} />
+      <Layer value={hours} size="50vh" zIndex={3} opacity={1} />
     </div>
   );
 };
