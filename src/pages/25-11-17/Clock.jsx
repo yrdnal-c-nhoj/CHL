15f2247780ad329d14a720cee9747ea9cbdaa61e
john-c-landry <johnclandry@yahoo.com>
2026-01-02 20:44:54 -0500

    b

--- src/pages/25-11-17/Clock.jsx
+++ src/pages/25-11-17/Clock.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
-import bg1 from "./mars2.webp";
-import bg2 from "./mars1.gif";
-import bg3 from "./mars1.gif";
-import bg4 from "./mars5.webp"; // ← added Mars four import
+import bg1 from "/assets/clocks/25-11-17/mars2.webp";
+import bg2 from "/assets/clocks/25-11-17/mars1.gif";
+import bg3 from "/assets/clocks/25-11-17/mars1.gif";
+import bg4 from "/assets/clocks/25-11-17/mars5.webp"; // ← added Mars four import
 const font2025_11_18 = '/fonts/25-11-17-mars.ttf';
 
 export default function MarsDigitalClock() {
