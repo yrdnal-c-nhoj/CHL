1441a061796957b3d1c24a81f92d8e286699626a
john-c-landry <johnclandry@yahoo.com>
2026-01-02 21:50:17 -0500

    x

--- src/pages/25-10-07/Clock.jsx
+++ src/pages/25-10-07/Clock.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react'
 
 // === Local assets ===
-import backgroundVideo from './big.mp4'
-import fallbackGif from './big.webp'
+import backgroundVideo from '/assets/clocks/25-10-07/big.mp4'
+import fallbackGif from '/assets/clocks/25-10-07/big.webp'
 const O251007font = '/fonts/25-10-07-str.ttf';
 
 export default function ImageAnalogClock () {
