2da3034e2aae84c0bf4de9b3b811e8c48f7f0e79
john-c-landry <johnclandry@yahoo.com>
2026-02-18 18:53:47 -0500

    z

--- src/pages/25-09/25-09-21/Clock.jsx
+++ src/pages/25-09/25-09-21/Clock.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { useFontLoader } from '../../../utils/fontLoader';import cust250921font from '../../../assets/fonts/25-09-21-ele.ttf?url';
 import stripe1 from '../../../assets/images/25-09/25-09-21/fire.gif?url'
 import stripe2 from '../../../assets/images/25-09/25-09-21/air.gif?url'
-import stripe3 from '../../../assets/images/25-09/25-09-21/h2o.gif?url'
+import stripe3 from '../../../assets/images/25-09/25-09-21/h2o.webp?url'
 import stripe4 from '../../../assets/images/25-09/25-09-21/earth.webp?url'
 
 export default function AnalogClock () {
