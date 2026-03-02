ee9dc6b58b681efe2875506fae67f9f9e003fc04
john-c-landry <johnclandry@yahoo.com>
2026-03-02 11:44:37 -0500

    x

--- src/pages/26-03/26-03-01/Clock.jsx
+++ src/pages/26-03/26-03-01/Clock.jsx
@@ -1,20 +1,52 @@
 import React from 'react';
+import westVideo from '../../../assets/images/26-03/26-03-01/west.mp4';
 
 const Clock = () => {
   return (
     <div style={{
       width: '100vw',
       height: '100dvh',
+      position: 'relative',
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
       backgroundColor: '#000',
-      color: '#fff',
-      fontSize: '4rem',
-      fontFamily: 'monospace'
+      overflow: 'hidden'
     }}>
-      <div>
-        26-03-01
+      {/* Background Video */}
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        style={{
+          position: 'absolute',
+          bottom: '0',
+          left: '-53%',
+          width: '180%',
+          height: 'auto',
+          minHeight: '100%',
+          objectFit: 'cover',
+          zIndex: 1,
+          filter: 'hue-rotate(-20deg) saturate(1.8) brightness(1.1)'
+        }}
+      >
+        <source src={westVideo} type="video/mp4" />
+      </video>
+      
+      {/* Content Overlay */}
+      <div style={{
+        position: 'relative',
+        zIndex: 10,
+        color: '#fff',
+        fontSize: '4rem',
+        fontFamily: 'monospace',
+        textAlign: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        padding: '20px 40px',
+        borderRadius: '10px'
+      }}>
+   srthewt
       </div>
     </div>
   );
