59a4c18d4d914d90363a869bc9f7d655c172b02b
john-c-landry <johnclandry@yahoo.com>
2026-03-05 09:33:59 -0500

    z

--- src/pages/25-12/25-12-07/Clock.jsx
+++ src/pages/25-12/25-12-07/Clock.jsx
@@ -6,7 +6,7 @@ import * as THREE from 'three'
 import backgroundImage from '../../../assets/images/25-12/25-12-07/h2o.webp'
 import backgroundImage2 from '../../../assets/images/25-12/25-12-07/water.gif'
 import font251207 from '../../../assets/fonts/25-12-07-isoca.ttf?url';
-import useFontLoader from './useFontLoader'
+import useFontLoader from '../../../utils/useFontLoader'
 
 export default function IcosahedronScene() {
   // Load font safely
