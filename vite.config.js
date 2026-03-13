ae513173535ce40043e86eed0604b641fa745707
john-c-landry <johnclandry@yahoo.com>
2026-03-13 10:42:47 -0400

    x

--- vite.config.js
+++ vite.config.js
@@ -29,8 +29,8 @@ export default defineConfig({
           if (/\.(woff|woff2|eot|ttf|otf)$/i.test(assetInfo.name)) {
             return 'assets/fonts/[name]-[hash][extname]';
           }
-          // Group all common image types into the assets/images folder
-          if (/\.(gif|jpe?g|png|svg|webp|avif)$/i.test(assetInfo.name)) {
+          // Group all common image types and videos into the assets/images folder
+          if (/\.(gif|jpe?g|png|svg|webp|avif|mp4|webm)$/i.test(assetInfo.name)) {
             return 'assets/images/[name]-[hash][extname]';
           }
           return 'assets/[name]-[hash][extname]';
