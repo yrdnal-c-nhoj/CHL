cc0b5700fb4151f9425c355c1d60204ed511dde8
john-c-landry <johnclandry@yahoo.com>
2026-03-29 14:00:44 -0400

    x

--- src/pages/26-03/26-03-26/Clock.tsx
+++ src/pages/26-03/26-03-26/Clock.tsx
@@ -1,31 +1,37 @@
 import React, { useMemo } from 'react';
-import { useSuspenseFontLoader } from '../../../utils/fontLoader';
+import { useMultipleFontLoader } from '../../../utils/fontLoader';
 import { useSecondClock } from '../../../utils/useSmoothClock';
-import type { FontConfig } from '../../../types/clock';
 import highwayBg from '../../../assets/images/26-03/26-03-26/highway.webp';
-import highwayFont from '../../../assets/fonts/26-03-26-highway.otf?url';
+import overFont from '../../../assets/fonts/26-03-26-over.otf';
 import styles from './Clock.module.css';
 
 const Clock: React.FC = () => {
   const time = useSecondClock();
 
-  // Font configuration - using the date-corresponding highway font
-  const fontConfigs = useMemo<FontConfig[]>(() => [
-    { fontFamily: 'HighwayFont', fontUrl: highwayFont }
-  ], []);
+  const fontConfigs = [
+    {
+      fontFamily: 'OverFont',
+      fontUrl: overFont,
+    }
+  ];
+  const fontsLoaded = useMultipleFontLoader(fontConfigs);
 
-  // This hook suspends the component until fonts are loaded
-  useSuspenseFontLoader(fontConfigs);
+  const fontFamily = fontsLoaded ? 'OverFont, Courier New, monospace' : 'Courier New, monospace';
 
   const timeDigits = useMemo(() => {
+    const rawHours = time.getHours();
+    const isPm = rawHours >= 12;
+    const hours12 = rawHours === 0 ? 12 : rawHours > 12 ? rawHours - 12 : rawHours;
+
     const formatTimeUnit = (unit: number) => unit.toString().padStart(2, '0');
 
-    const hours = formatTimeUnit(time.getHours());
+    const hours = hours12.toString();
     const minutes = formatTimeUnit(time.getMinutes());
 
     return {
       hours: hours.split(''),
       minutes: minutes.split(''),
+      amPm: isPm ? 'PM' : 'AM',
     };
   }, [time]);
 
@@ -33,25 +39,20 @@ const Clock: React.FC = () => {
     <div className={styles.container} style={{ '--bg-image': `url(${highwayBg})` } as React.CSSProperties}>
       <div className={styles.background} />
 
-      
       <div className={styles.clockWrapper}>
-        <div className={styles.timeUnit}>
-          {timeDigits.hours.map((digit, index) => (
-            <span key={`h-${index}`} className={styles.digit}>
-              {digit}
-            </span>
-          ))}
-        </div>
-        
-        {/* <span className={styles.separator}>:</span> */}
-        
-        <div className={styles.timeUnit}>
-          {timeDigits.minutes.map((digit, index) => (
-            <span key={`m-${index}`} className={styles.digit}>
-              {digit}
-            </span>
-          ))}
-        </div>
+        {timeDigits.hours.map((digit, index) => (
+          <span key={`h-${index}`} className={styles.digit} style={{ fontFamily }}>
+            {digit}
+          </span>
+        ))}
+        {timeDigits.minutes.map((digit, index) => (
+          <span key={`m-${index}`} className={styles.digit} style={{ fontFamily }}>
+            {digit}
+          </span>
+        ))}
+        <span className={styles.amPm} style={{ fontFamily }}>
+          {timeDigits.amPm}
+        </span>
       </div>
     </div>
   );
