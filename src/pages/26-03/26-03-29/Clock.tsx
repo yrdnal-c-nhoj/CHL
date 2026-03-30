1a7e1ef21e5fa54fbc3b2d707dda667ab6c94a82
john-c-landry <johnclandry@yahoo.com>
2026-03-29 20:53:27 -0400

    bz

--- src/pages/26-03/26-03-29/Clock.tsx
+++ src/pages/26-03/26-03-29/Clock.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useMemo } from 'react';
 import { useMultipleFontLoader } from '../../../utils/fontLoader';
-import bgVideo from '../../../assets/images/26-03/26-03-29/sunrise.mp4';
+import bgVideo from '../../../assets/images/26-03/26-03-29/sunrise1.mp4';
 import borderImage from '../../../assets/images/26-03/26-03-29/horse.webp';
 import scarabImage from '../../../assets/images/26-03/26-03-29/scarab.webp';
 import eastFont from '../../../assets/fonts/26-03-29-east.ttf';
