bb761acac77f56d57d4066b9a9b8658426873901
john-c-landry <johnclandry@yahoo.com>
2026-04-22 13:45:00 -0400

    x

--- src/pages/2026/26-04/26-04-20/Clock.tsx
+++ src/pages/2026/26-04/26-04-20/Clock.tsx
@@ -40,6 +40,7 @@ const Clock: React.FC = () => {
     backgroundImage: `url(${bgImg})`,
     backgroundSize: 'cover',
     backgroundPosition: 'center',
+    transform: 'scaleX(-1)',
     zIndex: 0,
   };
 
@@ -60,7 +61,7 @@ const Clock: React.FC = () => {
     transformOrigin: 'bottom center',
     transform: `translateX(-50%) rotate(${deg}deg)`,
     borderRadius: '2px',
-    boxShadow: '13px 0 4px #EC0B0B',
+    boxShadow: '13px 0 1px #EC0B0B',
   });
 
   const centerDotStyle: React.CSSProperties = {
