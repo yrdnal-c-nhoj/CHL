78832d982ef29518417a4b4a437dbc242ec3472e
john-c-landry <johnclandry@yahoo.com>
2026-04-28 13:25:36 -0400

    x

--- src/pages/2025/25-12/25-12-03/Clock.tsx
+++ src/pages/2025/25-12/25-12-03/Clock.tsx
@@ -2,11 +2,11 @@ import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { useSuspenseFontLoader } from '@/utils/fontLoader';
 import { useClockTime } from '@/utils/hooks';
 import type { FontConfig } from '@/types/clock';
-import font_2024_12_05 from '@/assets/fonts/2025/25-12-03-dog.ttf?url';
+import font_25_12_03 from '@/assets/fonts/2025/25-12-03-dog.ttf?url';
 import styles from './Clock.module.css';
 
 export const fontConfigs: FontConfig[] = [
-  { fontFamily: 'CustomFont', fontUrl: font_2024_12_05 }
+  { fontFamily: 'CustomFont', fontUrl: font_25_12_03 }
 ];
 
 const PuppyClockComponent: React.FC = () => {
