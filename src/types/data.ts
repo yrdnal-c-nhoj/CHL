6fdc934efea4f22a52ab17608f94ebc0874d3bd2
john-c-landry <johnclandry@yahoo.com>
2026-05-01 16:28:58 -0400

    x

--- src/types/data.ts
+++ src/types/data.ts
@@ -2,10 +2,11 @@ export interface ClockItem {
   path: string;
   date: string;
   title: string;
+  clockNumber?: number;
 }
 
 export interface DataContextType {
   items: ClockItem[];
   loading: boolean;
-  error?: string | null;
+  error: string | null;
 }
