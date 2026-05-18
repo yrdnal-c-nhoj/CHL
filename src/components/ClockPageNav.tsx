ba2a081d47388e7e9f670186209763647cdcdcf6
john-c-landry <johnclandry@yahoo.com>
2026-05-17 20:00:40 -0400

    b

--- src/components/ClockPageNav.tsx
+++ src/components/ClockPageNav.tsx
@@ -153,7 +153,8 @@ const ClockPageNav = ({
       >
         <div className={styles.footerCenter}>
           <span className={styles.footerDate}>
-            {formatDate(currentItem.date).replace(/\s?'/, " '")}
+            {formatDate(currentItem.date).replace(/\s*'/, "   '").replace(/^(\w+)/, '$1 ')}
+            {formatDate(currentItem.date).replace(/\s*'/, "   ' ").replace(/^(\w+)/, '$1 ')}
           </span>
           <span className={styles.footerTitle}>
             {formatTitle(currentItem.title)}
