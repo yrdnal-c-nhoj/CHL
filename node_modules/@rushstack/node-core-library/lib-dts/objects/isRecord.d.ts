04772fdfcdb57561cf9c5c9ce68558bf300f6cc9
john-c-landry <johnclandry@yahoo.com>
2026-05-22 19:25:10 -0400

    chore: stop tracking node_modules/

--- node_modules/@rushstack/node-core-library/lib-dts/objects/isRecord.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Returns `true` if `value` is a non-null, non-array plain object (i.e. assignable to
- * `Record<string, unknown>`), narrowing the type accordingly.
- *
- * @public
- */
-export declare function isRecord(value: unknown): value is Record<string, unknown>;
-//# sourceMappingURL=isRecord.d.ts.map
