03377ce9bd40fdb851af1f0e2163f41439513cfb
john-c-landry <johnclandry@yahoo.com>
2026-05-22 19:25:10 -0400

    chore: stop tracking node_modules/

--- node_modules/@rushstack/node-core-library/lib-dts/objects/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { areDeepEqual } from './areDeepEqual';
-export { isRecord } from './isRecord';
-export { type MergeWithCustomizer, mergeWith } from './mergeWith';
-//# sourceMappingURL=index.d.ts.map
