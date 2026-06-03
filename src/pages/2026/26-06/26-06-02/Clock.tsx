e6ea8dc0b37e82e3893b9d904302b74b771cc6c2
john-c-landry <johnclandry@yahoo.com>
2026-06-03 17:47:42 -0400

    x

--- src/pages/2026/26-06/26-06-02/Clock.tsx
+++ src/pages/2026/26-06/26-06-02/Clock.tsx
@@ -2,3 +2,7 @@
 import Clock, { assets as ClockAssets } from './Clock';
 export default Clock;
 export const assets = ClockAssets;
+// Forward everything to the canonical PascalCase file to avoid circular imports and case-collision errors
+import CanonicalClock, { assets as CanonicalAssets } from './Clock';
+export default CanonicalClock;
+export const assets = CanonicalAssets;
