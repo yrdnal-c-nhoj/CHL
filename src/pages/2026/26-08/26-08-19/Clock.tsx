296c128f390c068fa2b345e59f0729f0a96f2214
john-c-landry <johnclandry@yahoo.com>
2026-08-19 08:46:27 -0400

    c

--- src/pages/2026/26-08/26-08-19/Clock.tsx
+++ src/pages/2026/26-08/26-08-19/Clock.tsx
@@ -84,13 +84,6 @@ const AnalogClockComponent: React.FC = () => {
     transform: `rotate(${secAngle}deg)`,
   }), [secAngle]);
 
-  const digitalTime = useMemo(() => {
-    const hours = String(time.getHours()).padStart(2, '0');
-    const minutes = String(time.getMinutes()).padStart(2, '0');
-    const seconds = String(time.getSeconds()).padStart(2, '0');
-    return { hours, minutes, seconds };
-  }, [time]);
-
   return (
     // E. Root element must be `<main>`.
     <main
@@ -133,17 +126,6 @@ const AnalogClockComponent: React.FC = () => {
           className={`${styles.hand} ${styles.secondHand}`}
           style={secondHandStyle}
         />
-      </div>
-
-      <div className={styles.digitalClock}>
-        <span className={styles.digit}>{digitalTime.hours[0]}</span>
-        <span className={styles.digit}>{digitalTime.hours[1]}</span>
-        <span className={styles.colon}>:</span>
-        <span className={styles.digit}>{digitalTime.minutes[0]}</span>
-        <span className={styles.digit}>{digitalTime.minutes[1]}</span>
-        <span className={styles.colon}>:</span>
-        <span className={styles.digit}>{digitalTime.seconds[0]}</span>
-        <span className={styles.digit}>{digitalTime.seconds[1]}</span>
       </div>
     </main>
   );
