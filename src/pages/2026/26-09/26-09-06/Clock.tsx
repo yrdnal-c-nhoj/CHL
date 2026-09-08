348dbe602e5578eeecd06e2898c46de2d7e764fb
john-c-landry <johnclandry@yahoo.com>
2026-09-08 16:11:22 -0400

    x

--- src/pages/2026/26-09/26-09-06/Clock.tsx
+++ src/pages/2026/26-09/26-09-06/Clock.tsx
@@ -1,13 +1,44 @@
-import React from 'react';
-import CurrentTime from './CurrentTime';
+import { useClock } from '@/utils/hooks';
+import styles from './Clock.module.css';
 
-const App: React.FC = () => {
+const Clock_26_09_06 = () => {
+  const time = useClock();
+
+  const hours = time.getHours().toString().padStart(2, '0');
+  const minutes = time.getMinutes().toString().padStart(2, '0');
+  const seconds = time.getSeconds().toString().padStart(2, '0');
+
+  const fullTimeString = `${hours}:${minutes}:${seconds}`;
+
   return (
-    <div>
-      <h1>React Time Display</h1>
-      <CurrentTime />
-    </div>
+    <main className={styles.container}>
+      <div className={styles.clock}>
+        <div className={styles.timeGroup}>
+          {hours.split('').map((d, i) => (
+            <span key={`h${i}`} className={styles.digitBox}>{d}</span>
+          ))}
+        </div>
+        <span className={styles.colon}>:</span>
+        <div className={styles.timeGroup}>
+          {minutes.split('').map((d, i) => (
+            <span key={`m${i}`} className={styles.digitBox}>{d}</span>
+          ))}
+        </div>
+        <span className={styles.colon}>:</span>
+        <div className={styles.timeGroup}>
+          {seconds.split('').map((d, i) => (
+            <span key={`s${i}`} className={styles.digitBox}>{d}</span>
+          ))}
+        </div>
+      </div>
+
+      <time dateTime={time.toISOString()} className={styles.srOnly}>
+        {fullTimeString}
+      </time>
+    </main>
   );
 };
 
-export default App;
+Clock_26_09_06.displayName = 'Clock_26_09_06';
+
+export default Clock_26_09_06;
