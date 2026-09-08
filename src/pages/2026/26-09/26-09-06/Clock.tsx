f95c8fe23611bec3e66555bf96c8bbddb5e77d87
john-c-landry <johnclandry@yahoo.com>
2026-09-08 12:03:26 -0400

    m

---
+++ src/pages/2026/26-09/26-09-06/Clock.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import CurrentTime from './CurrentTime';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>React Time Display</h1>
+      <CurrentTime />
+    </div>
+  );
+};
+
+export default App;
