82d25af203a8736f8b91f07d9d96041ce0117b30
john-c-landry <johnclandry@yahoo.com>
2026-09-20 13:06:12 -0400

    x

--- src/pages/2025/25-07/25-07-03/Clock.tsx
+++ src/pages/2025/25-07/25-07-03/Clock.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { useSuspenseFontLoader } from '@/utils/fontLoader';
-import rocketGif from '@/assets/images/25_images/25-07/25-07-03/rocket.gif';
+import rocketGif from '@/assets/images/25_images/25-07/25-07-03/rocket.webp';
 import rockFont from '@/assets/fonts/25fonts/25-07-03-rock.ttf';
 export const assets = [rocketGif, rockFont];
 
