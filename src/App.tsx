6827a1544092db7185f69cb20c76fc495d0b30af
john-c-landry <johnclandry@yahoo.com>
2026-09-22 10:36:58 -0400

    x

--- src/App.tsx
+++ src/App.tsx
@@ -91,6 +91,9 @@ const AnalyticsAndSEO = React.memo(() => {
     if (!isClockPage) {
       document.body.style.overflow = 'auto';
       document.documentElement.style.overflow = 'auto';
+    } else {
+      document.body.style.overflow = 'hidden';
+      document.documentElement.style.overflow = 'hidden';
     }
   }, [processedPath.isDynamicClock, location.pathname]);
 
